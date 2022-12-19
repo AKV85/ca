a515f586684fe4b8f7de7be1d19ce469e4398a47
AKV85 <a.kotov.laknojus@gmail.com>
2022-12-19 14:55:16 +0200

    Ikeliu jsEventListeners 1 uzduoti

---
+++ js/jsEL.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("myButton");
+button.addEventListener("click", function () {
+    let input = document.getElementById("myInput");
+    input.value = "Labas, pasauli!";
+});
